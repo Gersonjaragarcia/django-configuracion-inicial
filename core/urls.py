24afb74bb4c88d7d37a596dd9bccdd1224b437f2
Gerson Jara <46004475+Gersonjaragarcia@users.noreply.github.comQ>
2026-02-27 22:24:19 -0500

    Configuramos las urls de blog y contacto

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main.views import home # Importamos nuestra vista
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), # Ruta raíz
+    path('', include('main.urls')),      # Esto ya se encarga de la raíz
+    path('blog/', include('blog.urls')),
+    path('contacto/', include('contacto.urls')), # Si creas la de contacto
 ]
+
